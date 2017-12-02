b2211969589c823f4d34009a96163eba1410de7b
Kutsak Nina <nina_svetik@mail.ru>
2017-12-02 11:19:21 +0300

    echo bot

--- bot.py
+++ bot.py
@@ -3,9 +3,15 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(func=lambda message: True, content_types=["text"]) #реагирование на сообщение от юзера 'text'
-def repeat_all_messages(message):
+@bot.message_handler(commands=["start"]) #реагирование на начало
+def start(message):
+    bot.send_message(message.chat.id, "Введите телефон в формате: /tel 7903......")
+
+@bot.message_handler(commands=["tel"]) #реагирование на начало
+def tel(message):
+    msisdn = message.text
     bot.send_message(message.chat.id, message.text)
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
