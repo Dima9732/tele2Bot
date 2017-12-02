df7609de7430f78eb27359abe03e12242cb111a0
Kutsak Nina <nina_svetik@mail.ru>
2017-12-02 11:54:13 +0300

    auth

--- bot.py
+++ bot.py
@@ -1,17 +1,37 @@
 import config
 import telebot
+import requests
 
 bot = telebot.TeleBot(config.token)
 
+class Client():
+    msisdn = None
+    Token = None
+
+client = Client()
+host = "http://tele2-hackday-2017.herokuapp.com/api/"
 @bot.message_handler(commands=["start"]) #реагирование на начало
 def start(message):
     bot.send_message(message.chat.id, "Введите телефон в формате: /tel 7903......")
 
-@bot.message_handler(commands=["tel"]) #реагирование на начало
+@bot.message_handler(commands=["tel"])
 def tel(message):
-    msisdn = message.text
-    bot.send_message(message.chat.id, message.text)
+    if len(message.text[5:]) == 11 and message.text[5:].isdigit():
+        client.msisdn = message.text[5:]
+        bot.send_message(message.chat.id, "А теперь парольное слово /pas .......")
+    else:
+        bot.send_message(message.chat.id, "Неправильный номер((")
 
+@bot.message_handler(commands=["pas"])
+def pas(message):
+    headers = {'accept': 'application/json', 'X-API-Token':message.text[5:]}
+    r = requests.get(host + "/subscribers/" + client.msisdn + "/balance")
+    print(r.url)
+    if r.status_code == 200:
+        client.Token = message.text[5:]
+        bot.send_message(message.chat.id, r.json())
+    else:
+        bot.send_message(message.chat.id, "Неправильный пароль(( " + message.text[5:])
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
